38973ce621f5bee1cee85d199435ea2a7e7afd58
Phat3861-hub <nguyentanphat03122004@gmail.com>
2026-03-31 08:18:32 +0700

    fix: update docker file

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,18 @@
+// import axios from "axios";
+
+// export const http = axios.create({
+//   baseURL: "http://localhost:9090/api/", // Chú ý: Chỉ để /api
+//   withCredentials: true,
+//   headers: {
+//     "Content-Type": "application/json",
+//     "X-Client-Type": "WEB",
+//   },
+// });
+
 import axios from "axios";
 
 export const http = axios.create({
-  baseURL: "http://localhost:9090/api",
+  baseURL: "/api/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
